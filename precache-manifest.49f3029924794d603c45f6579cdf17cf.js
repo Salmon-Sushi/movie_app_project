b9c8b32fb250407fd9a13086f24939b686729825
SalmonSushi <stephen_shin@naver.com>
2020-08-07 19:18:05 +0900

    Updates

---
+++ precache-manifest.49f3029924794d603c45f6579cdf17cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a42cecf76113e79afe9bfd93e0175081",
+    "url": "/movie_app_project/index.html"
+  },
+  {
+    "revision": "edb9127f5ddcb12b5fc3",
+    "url": "/movie_app_project/static/css/main.067c1c56.chunk.css"
+  },
+  {
+    "revision": "2474c8243e3303075615",
+    "url": "/movie_app_project/static/js/2.4cb4d100.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_project/static/js/2.4cb4d100.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edb9127f5ddcb12b5fc3",
+    "url": "/movie_app_project/static/js/main.e8e9af12.chunk.js"
+  },
+  {
+    "revision": "6a0fbbf76fa4cb057143",
+    "url": "/movie_app_project/static/js/runtime-main.c5d6975c.js"
+  }
+]);
